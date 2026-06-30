8a8ffbc0325b80b0481a37cb3dca1d5d2f94dbf7
Willian Nomura <willian.nomura@hotmail.com>
2026-06-30 21:33:44 +0100

    complex

--- algorithm/complex_algorithm.c
+++ algorithm/complex_algorithm.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   complex_algorithm.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: romdo-na <romdo-na@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: willpere <willpere@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 15:14:15 by romdo-na          #+#    #+#             */
-/*   Updated: 2026/06/30 15:35:07 by romdo-na         ###   ########.fr       */
+/*   Updated: 2026/06/30 21:21:21 by willpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,35 @@
 
 void		radix(t_stack **stack_a, t_stack **stack_b, t_bench *bench)
 {
-    stack_a = NULL;
-    stack_b = NULL;
-    bench = NULL;
+	int	size;
+	int	max_bit;
+	int	max_rank;
+	int	i;
+	int	j;
+
+	size = stack_size(*stack_a);
+	max_rank = size;
+	max_bit = 0;
+	
+	while (max_rank > 0)
+	{
+		max_rank >>= 1;
+		max_bit++;
+	}
+	i = 0;
+	while (i < max_bit)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (((*stack_a)->rank >> i) & 1)
+				ra(stack_a, bench);
+			else
+				pb(stack_a, stack_b, bench);
+			j++;
+		}
+		while (*stack_b)
+			pa(stack_a, stack_b, bench);
+		i++;
+	}	
 }
